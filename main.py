54a0862af44d392a5a6ad93c7b6ed20f70620bc9
Bramvlaere <Bram.vanlaere@gmail.com>
2022-06-29 13:40:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__,template_folder=TEMPLATE_PATH)
 def first():
     return render_template('index.html', title='Home')
 
-@app.route('/get_data')
+@app.route('/get_data', methods =["GET", "POST"])
 def get_data():
     new_calc=''
     print('in get_data')
